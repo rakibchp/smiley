60dc87531a1a96606852e93bd0e791f11fa48720
rakib <rakibchp@gmail.com>
2022-03-02 00:00:44 +0600

    apps main file uddated

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone_flutter/providers/user_provider.dart';
-import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
-import 'package:instagram_clone_flutter/responsive/responsive_layout.dart';
-import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
-import 'package:instagram_clone_flutter/screens/login_screen.dart';
-import 'package:instagram_clone_flutter/utils/colors.dart';
+import 'package:smiley/providers/user_provider.dart';
+import 'package:smiley/responsive/mobile_screen_layout.dart';
+import 'package:smiley/responsive/responsive_layout.dart';
+import 'package:smiley/responsive/web_screen_layout.dart';
+import 'package:smiley/screens/login_screen.dart';
+import 'package:smiley/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
